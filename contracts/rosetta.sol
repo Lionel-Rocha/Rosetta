88fccf11ab9994c8c9af9a8f27e7ddf47bc50511
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-05-16 19:17:35 -0300

    Adding change_username and get_user

--- contracts/rosetta.sol
+++ contracts/rosetta.sol
@@ -12,6 +12,7 @@ contract rosetta {
 
     struct stone {
         address writer;
+        string username;
         uint256 id;
         string text;
         uint256 timestamp;
@@ -27,19 +28,36 @@ contract rosetta {
     mapping(address => user) Users;
 
     event write_stone(address writer, uint256 id, string text, uint256 timestamp);
+    event change_user_det(address writer, string username);
+
+    function change_user_details(string memory name, string memory bio, string memory profile_picture) public {
+        user storage user_data = Users[msg.sender];
+        user_data.name = name;
+        user_data.bio = bio;
+        user_data.profile_picture = profile_picture;
+
+        emit change_user_det(msg.sender, name);
+    }
 
     function write_in_stone(string memory text) public {
         stone storage newStone = Stones[counter];
         newStone.text = text;
         newStone.writer = msg.sender;
+        newStone.username = Users[msg.sender].name;
         newStone.id = counter;
         newStone.timestamp = block.timestamp;
         emit write_stone(msg.sender, counter, text, block.timestamp);
         counter++;
     }
 
-    function get_stone(uint256 id) public view returns (stone[] memory){
-        stone[] memory result = new stone[](id);
-        return result;
+    function get_stone(uint256 id) public view returns (stone memory) {
+        return Stones[id];
     }
+
+    function get_user(address user_address) public view returns (user memory){
+        return Users[user_address];
+    }
+
+
+
 } 
