86cea039be3aeaa9122b532d1fb87c5129f0b4fb
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-05-18 13:56:50 -0300

    Wrapping things up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const port = process.env.PORT || 3000;
 const {parse} = require("querystring");
 const path = require('path');
@@ -42,48 +41,7 @@ async function get_user(address){
         return result;
     }
 }
-let ethersProvider, signer;
-async function write_stone(text) {
-    // return await contract.write_in_stone(text);
-    ethersProvider = new ethers.providers.Web3Provider(wallet.provider, sepolia_url)
-    signer = ethersProvider.getSigner()
-    contract = ethers.Contract("0xF744073FDE4d0f259624e675620587F69efA924C", contract_abi, ethersProvider);
-    return await contract.write_in_stone(text);
-}
 
-async function change_user_info(wallet, name, bio, profile_picture){
-    ethersProvider = new ethers.providers.Web3Provider(wallet.provider, sepolia_url)
-    signer = ethersProvider.getSigner()
-    contract = ethers.Contract("0xF744073FDE4d0f259624e675620587F69efA924C", contract_abi, ethersProvider);
-
-    const updates = {};
-
-    if (name && name.trim()) {
-        updates.name = name.trim();
-    }
-
-    if (bio && bio.trim()) {
-        updates.bio = bio.trim();
-    }
-
-    if (profile_picture && profile_picture.trim()) {
-        updates.profile_picture = profile_picture.trim();
-    }
-
-    if (Object.keys(updates).length === 0) {
-        throw new Error("No valid input provided to update.");
-    }
-    console.log(updates.name, updates.bio, updates.profile_picture);
-    try {
-         // This function should be defined to handle the actual update logic
-        return await contract.change_user_details(updates.name, updates.bio, updates.profile_picture);
-    } catch (error) {
-        console.error("Error updating user information:", error);
-        throw new Error("Failed to update user information.");
-    }
-}
-
-// Create a server object:
 http.createServer(async function (req, res) {
     try {
         const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
@@ -94,28 +52,7 @@ http.createServer(async function (req, res) {
             let result = await get_stones();
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(result));
-        } else if (pathname === '/write_stone' && method === 'POST' && parsedUrl.searchParams.has('wallet')) {
-            let user_wallet = parsedUrl.searchParams.get('wallet');
-            let body = '';
-
-            req.on('data', chunk => {
-                body += chunk.toString();
-            });
-
-            req.on('end', async () => {
-                const parsedBody = parse(body);
-                const text = parsedBody.text;
-
-                try {
-                    let result = await write_stone(user_wallet, text);
-                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(result));
-                } catch (error) {
-                    console.error("Error writing stone:", error);
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end("Internal Server Error");
-                }
-            });
+        
         } else if (pathname === '/get_user' && method === 'GET' && parsedUrl.searchParams.has('address')) {
             let user_address = parsedUrl.searchParams.get('address');
             let result = await get_user(user_address);
